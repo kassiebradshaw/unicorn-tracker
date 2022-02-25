b11031185e2579de987efad2cf8489b7bddebab5
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2022-02-24 21:52:59 -0800

    Almost where I want it

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import Unicorn from './Unicorn.js';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Main extends React.Component {
     render() {
         return (
-            this.props.unicornData.map((unicorn, idx) => 
-            <div key={idx}>
-                <Unicorn 
-                idx={idx}
-                name={unicorn.name}
-                favFood={unicorn.favFood}
-                color={unicorn.color}
-                location={unicorn.location}  
-                handleUpdate={this.props.handleUpdate}/>
+            <div id="main">
+
+                <Row xs={1} md={2} lg={3} id="Row"> 
+                    
+                    {this.props.unicornData.map((unicorn, idx) =>
+                    <Col>
+                        <Unicorn
+                            key={idx}
+                            idx={idx}
+                            unicorn={unicorn}
+                            name={unicorn.name}
+                            favFood={unicorn.favFood}
+                            color={unicorn.color}
+                            location={unicorn.location}
+                            imgUrl={unicorn.imgUrl}
+                            handleUpdate={this.props.handleUpdate}
+                        />
+                    </Col>
+                    )}
+                </Row>
             </div>
-        ))
+        )
     }
 }
 
